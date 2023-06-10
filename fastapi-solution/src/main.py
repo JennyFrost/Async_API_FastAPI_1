21653d916cf3d6ca27c7a3e920f7dbbdf1b49291
vladimir <vladimir1012kov@gmail.com>
2023-06-10 22:59:43 +0300

    Добавил сервис в docker-compose, рабочая сборка

--- fastapi-solution/src/main.py
+++ fastapi-solution/src/main.py
@@ -34,11 +34,11 @@ app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
 app.include_router(genres.router, prefix='/api/v1/genres', tags=['genres'])
 app.include_router(persons.router, prefix='/api/v1/persons', tags=['persons'])
 
-if __name__ == '__main__':
-    uvicorn.run(
-        'main:app',
-        host='0.0.0.0',
-        port=8000,
-        log_config=LOGGING,
-        log_level=logging.DEBUG,
-    )
+# if __name__ == '__main__':
+#     uvicorn.run(
+#         'main:app',
+#         host='0.0.0.0',
+#         port=8000,
+#         log_config=LOGGING,
+#         log_level=logging.DEBUG,
+#     )
