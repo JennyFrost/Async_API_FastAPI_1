fe14ec98787c28db752a98f4f90468a84600e075
JennyFost <jenny1515@mail.ru>
2023-06-13 04:53:56 +0300

    поменяла @app.on_event('startup') и @app.on_event('shutdown') на объект lifespan

--- fastapi-solution/src/main.py
+++ fastapi-solution/src/main.py
@@ -1,4 +1,4 @@
-import logging
+from contextlib import asynccontextmanager
 
 from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
@@ -7,27 +7,24 @@ from redis.asyncio import Redis
 
 from api.v1 import films, genres, persons
 from core.config import settings
-from core.logger import LOGGING
 from db import elastic, redis
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    redis.redis = Redis(host=settings.redis_host, port=settings.redis_port)
+    elastic.es = AsyncElasticsearch(hosts=[f'{settings.elastic_host}:{settings.elastic_port}'])
+    yield
+    await redis.redis.close()
+    await elastic.es.close()
+
 app = FastAPI(
+    lifespan=lifespan,
     title=settings.project_name,
     docs_url='/api/openapi',
     openapi_url='/api/openapi.json',
     default_response_class=ORJSONResponse,
 )
-
-
-@app.on_event('startup')
-async def startup():
-    redis.redis = Redis(host=settings.redis_host, port=settings.redis_port)
-    elastic.es = AsyncElasticsearch(hosts=[f'{settings.elastic_host}:{settings.elastic_port}'])
-
-
-@app.on_event('shutdown')
-async def shutdown():
-    await redis.redis.close()
-    await elastic.es.close()
 
 app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
 app.include_router(genres.router, prefix='/api/v1/genres', tags=['genres'])
